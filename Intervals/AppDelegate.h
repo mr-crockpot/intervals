19f1e1503573e44b90001a3d09ae5caa41d86afc
mr-crockpot <adamschor@adams-macbook-air.local>
2018-06-03 09:41:23 -0600

    Initial Commit

---
+++ Intervals/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Intervals
+//
+//  Created by Adam Schor on 6/3/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Intervals/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Intervals
+//
+//  Created by Adam Schor on 6/3/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
